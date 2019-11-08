abebad63a71618e3ae1e6f8147f0246af25c79cb
Yevishev Maxim <m.yevishev@delaweb.ru>
2019-11-08 19:08:45 +0400

    made empty controllers, made empty twig templates

---
+++ src/Controller/AuthViewController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class AuthViewController extends AbstractController
+{
+
+    use TargetPathTrait;
+
+    /**
+     * @Route("/", name="main_page")
+     */
+    public function mainPage()
+    {
+        if ($security->isGranted('ROLE_USER')){
+            return $this->redirectToRoute('profile_page');
+        }
+    }
+}
---
+++ src/Controller/ProfileViewController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Controller;
+
+
+
+class ProfileViewController
+{
+    /**
+     * @Route("/profile/{phone}", name="profile_page")
+     */
+}
