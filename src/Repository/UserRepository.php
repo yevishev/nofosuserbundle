f363f575dfcad2ff910c993780ccacc264140974
Yevishev Maxim <m.yevishev@delaweb.ru>
2019-12-16 17:17:15 +0400

    added comments

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,9 +19,6 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
     //Метод для вывода массива существующих пользователей при регистрации
     public function findAllUsers()
     {
